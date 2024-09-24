0c01a2d14c45ce87a5856d7d2240a220694ea6f7
Joe Meyer <17205605+djangojoe23@users.noreply.github.com>
2024-09-24 05:02:24 -0400

    getComputerChoice function is completed.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello")
+
+function getComputerChoice(){
+    randomNumber = Math.ceil(3*Math.random());
+    if (randomNumber === 1){
+        return 'rock';
+    }
+    else if(randomNumber === 2){
+        return 'paper';
+    }
+    else{
+        return 'scissors';
+    }
+}
+
+computerChoice = getComputerChoice()
